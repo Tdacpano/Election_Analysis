39f866f6a8e36f42f807e572e2510144673f8348
Tdacpano <117120227+Tdacpano@users.noreply.github.com>
2022-12-01 11:32:40 -0800

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -50,31 +50,42 @@ with open(file_to_load) as election_data:
     #add a vote to each count
         candidate_votes[candidate_name] += 1
 
+    # Save the results to our text file.
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal.  
+        Election_results = (
+            f"Election Results\n"
+            f"---------------------\n"
+            f"Total Votes:369,711\n"
+            f"---------------------\n")
+        print(Election_results, end="")
+    # Save the final vote count to the text file.
+        txt_file.write(Election_results)
 
 # Determine teh percentage of votes for each candidate
     #. Iterate through the candidate list
 for candidate_name in candidate_votes:
     #. Retrieve vote counnt of a candidate.
-    votes = candidate_votes[candidate_name]
+            votes = candidate_votes[candidate_name]
     #.Calculate the percentage of the vote count
-    vote_percentage = float(votes) / float(total_votes)*100
-   #  To do: print out the winning candidate, vote count and percentage to terminal.
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            vote_percentage = float(votes) / float(total_votes)*100
 
+   #  To do: print out the winning candidate, vote count and percentage to terminal.
+    # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
     #. Determine if the votes are greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
+if (votes > winning_count) and (vote_percentage > winning_percentage):
        #. If true then set winning_count = votes and winning_percent = vote_percentage.        
-            winning_percentage = votes 
-            winning_percentage = vote_percentage
+             winning_percentage = votes 
+             winning_percentage = vote_percentage
         #. Set the winning_candidate equal to the candidate's name.
-            winninng_candidate = candidate_name
+             winninng_candidate = candidate_name
 winning_candidate_summary = (
     f"-------------------------\n"
     f"Winner: {winninng_candidate}\n"
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
-print(winning_candidate_summary)      
+# print(winning_candidate_summary)      
 
 
 
